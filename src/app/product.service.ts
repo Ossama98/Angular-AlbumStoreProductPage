62fc437e8112263933173d457dff6e23f9d0705e
Ossama98 <ossama1998@windowslive.com>
2023-04-13 11:34:36 +0200

    zad"é

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import  { Http, Response } from '@angular/http'
-import 'rxjs/add/operator/map';
+import { Album } from './album';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private _albumUrl = "../assets/album.json";
+  private _albumUrl: string = "../assets/album.json";
 
-  constructor(private _http: Http) { }
+  constructor(private _http: HttpClient) { }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json())
+  getAlbum(id: number): Observable<Album>{
+    return this._http.get<Album>(this._albumUrl)
+    // .pipe(
+    //   map( (response) => console.log(typeof response))
+    // )
   }
 }
