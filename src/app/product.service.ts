16450865afe97fbdcd02e9cdfe94c3533b565763
Ossama98 <ossama1998@windowslive.com>
2023-04-13 11:19:05 +0200

    s

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import  { Http } from '@angular/http'
+import  { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class ProductService {
   constructor(private _http: HttpClient) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl)
+    return this._http.get(this._albumUrl).map((response) => response.json())
     // .pipe(
     //   map( (response) => console.log(typeof response))
     // )
